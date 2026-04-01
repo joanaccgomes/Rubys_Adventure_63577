805a17b142c1b6433e103dbc83b72ae4cff4669f
joanaccgomes <a63577@alunos.ipb.pt>
2026-04-01 16:32:48 +0100

    Health system
    all components completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,66 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-   
+//movement
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed =3.0f;
+
+//health
+    public int maxHealth =5;
+    int currentHealth;
+    public int health { get {return currentHealth; }}
+
+//temporary invincibility
+    public float timeInvicible =2.0f;
+    bool isInvencible;
+    float damageCooldown;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        
+
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-       
+        
+
+       if (isInvencible){
+        damageCooldown -= Time.deltaTime;
+        if (damageCooldown <0){
+            isInvencible=false;
+        }
+       }
 
     }
+
     void FixedUpdate(){
         Vector2 position = (Vector2)rigidbody2d.position + move* 3.0f * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+
+
+    public void ChangeHealth (int amount){
+       
+
+        if (amount <0){
+            if (isInvencible){
+                return;
+            }
+            isInvencible =true;
+            damageCooldown = timeInvicible;
+        }
+         currentHealth =Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
